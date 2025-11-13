0c6af6c62c9b5329fdd2f189f89874fee7c1dfda
Théo Lequesne <60904290+Jufevic@users.noreply.github.com>
2025-11-13 11:50:59 +0100

    Change year

--- create_new_day.py
+++ create_new_day.py
@@ -1,16 +1,18 @@
 import os
-from pathlib import Path
-from datetime import datetime
 import shutil
+from pathlib import Path
+
 from aocd import get_data
 
-day = datetime.now().day
+DAY = 2
+YEAR = 2021
+
 root_dir = Path(__file__).parent
-day_dir = root_dir / f"day_{day:02}"
+day_dir = root_dir / f"day_{DAY:02}"
 if not os.path.exists(day_dir):
     os.makedirs(day_dir)
     with open(Path(day_dir / "input.txt"), "w") as f:
-        f.write(get_data(day=day))
+        f.write(get_data(day=DAY, year=YEAR))
     Path(day_dir / "demo_input.txt").touch()
 
     shutil.copyfile(root_dir / "template.py", day_dir / "part_1.py")
