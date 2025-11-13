d8986e31508e8d6913be3a5b4efe125671e35c30
Théo Lequesne <60904290+Jufevic@users.noreply.github.com>
2025-11-13 15:04:50 +0100

    Solved day 2

---
+++ day_02/part_1.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from parse import parse
+
+CURRENT_FOLDER = Path(__file__).absolute().parent
+INPUT_FILE = CURRENT_FOLDER / "input.txt"
+DEMO_INPUT_FILE = CURRENT_FOLDER / "demo_input.txt"
+
+position = 0
+depth = 0
+
+with open(INPUT_FILE) as f:
+    for line in f.read().splitlines():
+        instruction, steps = parse("{} {:d}", line)
+        match instruction:
+            case "forward":
+                position += steps
+            case "down":
+                depth += steps
+            case "up":
+                depth -= steps
+
+print(depth * position)
